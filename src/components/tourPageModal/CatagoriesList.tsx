304c780e8396d39e93c01f9a6d5a1ec9cf162877
Hoàng Nguyễn Duy <you@example.com>
2024-10-09 23:08:07 +0700

    feat: Adding modal to tour page

---
+++ src/components/tourPageModal/CatagoriesList.tsx
@@ -0,0 +1,59 @@
+import React from 'react'
+import { Checkbox } from 'react-daisyui';
+const CatagoriesList: React.FC<object> = () => {
+    const List = [
+        {
+            name: 'Tour',
+            number: 92
+        },
+        {
+            name: 'Attractions',
+            number: 92
+        },
+        {
+            name: 'Day Trips',
+            number: 92
+        },
+        {
+            name: 'Outdoor Activities',
+            number: 92
+        },
+        {
+            name: 'Concert & Show',
+            number: 92
+        },
+        {
+            name: 'Indoor',
+            number: 92
+        },
+        {
+            name: 'Sight Seeing',
+            number: 92
+        },
+        {
+            name: 'Travels',
+            number: 92
+        },
+    ]
+    return (
+            <div className='space-y-2 mt-8 pl-1'>
+                {
+                    List.map((items, index) => (
+                        <div className='flex justify-between'>
+                            <label key={index}
+                                className='flex justify-between text-lg text-center text-stone-500 '>
+                                <div className='flex space-x-2 items-center '>
+                                    <div className='flex items-center justify-center rounded-lg cursor-pointer hover:bg-slate-200 w-8 h-8'><Checkbox className='w-5 h-5 accent-[#029e9d] cursor-pointer ' /></div>
+                                    <div className=''>{items.name}</div>
+                                </div>
+                            </label>
+                            <div>{items.number}</div>
+                        </div>
+                    ))
+                }
+            </div>
+       
+    )
+}
+
+export default CatagoriesList
--- src/components/website/tourPage/tourPage.tsx
+++ src/components/website/tourPage/tourPage.tsx
@@ -5,10 +5,15 @@ import { Select } from 'react-daisyui';
 import { SlCalender } from "react-icons/sl";
 import { FaStar } from 'react-icons/fa'
 import { product1, shape1 } from '../../../assets/tour/index';
+import CatagoriesList from '../../tourPageModal/catagoriesList'
+import DurationList from '../../tourPageModal/DurationList';
+import { FaArrowRightLong } from "react-icons/fa6";
 
 const TourPage: React.FC<object> = () => {
 
     const [rating] = useState(5)
+
+
 
     return (
         <div>
@@ -31,15 +36,23 @@ const TourPage: React.FC<object> = () => {
 
                 <div className='flex justify-center items-center flex-col mt-2'>
                     <div className='w-[70%] flex '>
-                        <div className='w-[30%]'>
-                            Catagories type
+
+                        <div className='w-[27%] pt-2 mr-7 flex flex-col '>
+                            <div>
+                                {/* Nav bar filter */}
+                                <p className='text-3xl font-[450] p-2 border-b-2 border-solid border-[#029e9d] '>Catagories Type</p>
+                                <CatagoriesList />
+                                <p className='text-3xl font-[450] p-2 border-b-2 border-solid border-[#029e9d] mt-8'>Duration Type</p>
+                                <DurationList />
+                            </div>
                         </div>
+
                         <div className='w-[70%]'>
                             <div className='h-16 flex justify-between items-center'>
                                 <p className='text-stone-500 text-lg'>Showing 1-5 of 80 results</p>
                                 <div className='flex'>
                                     <button className='w-12 h-12 bg-yellow-400 rounded-lg flex justify-center items-center'><LuMenu className='text-white w-7 h-7' /></button>
-                                    <Select defaultValue={'default'} className='ml-3 border border-solid w-40 h-12 rounded-xl pl-4 hover:border-slate-400' >
+                                    <Select defaultValue={'default'} className='ml-3 border border-solid w-40 h-12 rounded-xl pl-4 hover:border-slate-400 cursor-pointer' >
                                         <option value="default">Sort By</option>
                                         <option value="rating">Rating</option>
                                         <option value="low">Price: Low to high</option>
@@ -48,38 +61,42 @@ const TourPage: React.FC<object> = () => {
                                 </div>
                             </div>
                             <div className='gap-8 grid grid-cols-2'>
-                            {Array.from({ length: 5 }).map(() => (
-                                 <div className=' rounded-xl drop-shadow-lg border border-solid '>
-                                 <div className='relative flex items-center justify-center w-[100%] '>
-                                     <div className='overflow-hidden w-full h-full rounded-t-xl '>
-                                         <img src={product1} className='rounded-t-xl transition-transform duration-300 hover:scale-110' alt="" />
-                                     </div>
-                                     <div className='absolute right-6 top-[90%] flex w-40 h-12 bg-[#029e9d] rounded-xl items-center justify-center'>
-                                         <SlCalender className='w-6 h-6 mr-2 text-white' />
-                                         <p className='text-white font-bold text-lg'>9 Days Tours</p>
-                                     </div>
-                                 </div>
-                                 <div className='p-5'>
-                                     <p className='text-[#029e9d] font-medium text-lg'>Greece</p>
-                                     <Link to={'/home'} className='text-2xl font-semibold'>Santorini, Oia</Link>
-                                     <div className='flex items-center mt-1'>
-                                         <div className='flex flex-row space-x-1 pt-2 mb-2'>
-                                             {Array.from({ length: 5 }).map((_, index) => (
-                                                 <div key={index}>
-                                                     <FaStar className={`w-4 h-4  ${index < rating ? 'text-yellow-400' : 'text-slate-200'}`} />
-                                                 </div>
-                                             ))}
-                                         </div>
-                                         <p className='ml-2'>(12)</p>
-                                     </div>
-                                     <p className='mt-3 w-[92%] h-24 text-lg font-extralight'>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum</p>
-                                     <div className='flex items-center mt-1 mb-3'>
-                                         <label className='text-xl font-bold text-[#029e9d]'>$170.00</label>
-                                         <p className='ml-2 font-extralight text-base'>| Per person</p>
-                                     </div>
-                                 </div>
-                             </div>
-                            ))}                            
+                                {Array.from({ length: 6 }).map(() => (
+                                    <div className=' rounded-xl drop-shadow-lg border border-solid hover:shadow-inner hover:shadow-sm hover:shadow-[#029e9d]'>
+                                        <div className='relative flex items-center justify-center w-[100%] '>
+                                            <div className='overflow-hidden w-full h-full rounded-t-xl '>
+                                                <img src={product1} className='rounded-t-xl transition-transform duration-300 hover:scale-110' alt="" />
+                                            </div>
+                                            <div className='absolute right-6 top-[90%] flex w-40 h-12 bg-[#029e9d] rounded-xl items-center justify-center'>
+                                                <SlCalender className='w-6 h-6 mr-2 text-white' />
+                                                <p className='text-white font-bold text-lg'>9 Days Tours</p>
+                                            </div>
+                                        </div>
+                                        <div className='p-5'>
+                                            <p className='text-[#029e9d] font-medium text-lg'>Greece</p>
+                                            <Link to={'/home'} className='text-2xl font-semibold'>Santorini, Oia</Link>
+                                            <div className='flex items-center mt-1'>
+                                                <div className='flex flex-row space-x-1 pt-2 mb-2'>
+                                                    {Array.from({ length: 5 }).map((_, index) => (
+                                                        <div key={index}>
+                                                            <FaStar className={`w-4 h-4  ${index < rating ? 'text-yellow-400' : 'text-slate-200'}`} />
+                                                        </div>
+                                                    ))}
+                                                </div>
+                                                <p className='ml-2'>(12)</p>
+                                            </div>
+                                            <p className='mt-3 w-[92%] h-24 text-lg font-extralight'>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum</p>
+                                            <div className='flex items-center mt-1 mb-3'>
+                                                <label className='text-xl font-bold text-[#029e9d]'>$170.00</label>
+                                                <p className='ml-2 font-extralight text-base'>| Per person</p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                ))}
+                            </div>
+                            <div className='flex justify-center mt-8'>
+                                <button className='flex items-center w-32 justify-center h-12 text-white rounded-lg bg-[#029e9d]'>Load more <FaArrowRightLong className='ml-1'/>
+                                </button>
                             </div>
                         </div>
                     </div>
