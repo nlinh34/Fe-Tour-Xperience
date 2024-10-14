a4863e290cae81a8038cd378dcf6570d7365261f
nlinh34 <linhhnnpi00217@gmail.com>
2024-10-15 03:54:39 +0700

    chore: update landing page

--- src/pages/static/LandingPage.tsx
+++ src/pages/static/LandingPage.tsx
@@ -16,7 +16,7 @@ const LandingPage: React.FC<object> = () => {
             nhấn mạnh vào trải nghiệm cá nhân, giúp người dùng có những kỷ niệm đáng nhớ
             từ những chuyến đi đó.
           </p>
-          <Link to='/home-page' >
+          <Link to='/home' >
             <button className='text-white border border-solid-2 px-5 py-2 border-pink-800 mt-5 transition transform hover:scale-110 duration-700 hover:bg-pink-500 font-semibold'>
               <div className='flex flex-row items-center gap-1'>
                 <p>TRẢI NGHIỆM</p>
